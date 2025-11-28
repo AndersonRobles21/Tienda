a48a326238d050cfca6d5c723e3d86cc54e8a81c
Anderson <andersontopaga2018@gmail.com>
2025-11-28 12:29:47 -0500

    intento de render

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
-urls from django.urls import path, include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register(r'', OrderViewSet, basename='order')
+router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = [
     path('', include(router.urls)),
