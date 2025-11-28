68ce19d8ca2b9a5387abac545e5e1030c994db94
lFonseca117 <laufonseca727@gmail.com>
2025-11-28 11:59:56 -0500

    Update urls.py

--- settings/urls.py
+++ settings/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for shop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -21,7 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 def api_home(request):
-    """Vista raíz de API que devuelve un mensaje de bienvenida."""
     return JsonResponse({'message': 'Welcome to Shop API', 'status': 'running'})
 
 urlpatterns = [
@@ -33,14 +16,11 @@ urlpatterns = [
     path('products/', views.products, name='products'),
     path('orders/', views.orders, name='orders'),
 
-    # API Endpoints
-    path('api/', api_home, name='api_home'),
+    # JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/products/', include('products.urls')),
-    path('api/orders/', include('orders.urls')),
-    
-    # Rutas alternativas sin /api/
-    path('products-api/', include('products.urls')),
-    path('orders-api/', include('orders.urls')),
-]
 
+    # API REST REAL
+    path('api/products/', include('products.api_urls')),
+    path('api/orders/', include('orders.api_urls')),
+]
