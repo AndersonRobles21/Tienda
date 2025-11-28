630db774f374e8ce23a8b51b9cc2b678c74e62d6
lFonseca117 <laufonseca727@gmail.com>
2025-11-28 11:56:31 -0500

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,8 +2,16 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Product
 from .serializers import ProductSerializer
+from django.shortcuts import render
 
+
+# --- API ---
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
+
+
+# --- Página HTML ---
+def products_page(request):
+    return render(request, 'products.html')
